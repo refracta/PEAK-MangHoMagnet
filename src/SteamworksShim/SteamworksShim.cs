b57e528a7b29b31ef29eae3403af8f0fdaafb2fe
refracta <refracta@abstr.net>
2025-12-29 18:48:39 +0900

    Fix Steamworks assembly mismatch with shim

---
+++ src/SteamworksShim/SteamworksShim.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace Steamworks
+{
+    public struct CSteamID
+    {
+        public ulong m_SteamID;
+
+        public CSteamID(ulong steamId)
+        {
+            m_SteamID = steamId;
+        }
+    }
+
+    public struct LobbyDataUpdate_t
+    {
+        public ulong m_ulSteamIDLobby;
+        public ulong m_ulSteamIDMember;
+        public byte m_bSuccess;
+    }
+
+    public sealed class Callback<T>
+    {
+        public static Callback<T> Create(Action<T> action)
+        {
+            return new Callback<T>();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public static class SteamAPI
+    {
+        public static void RunCallbacks()
+        {
+        }
+    }
+
+    public static class SteamMatchmaking
+    {
+        public static int GetNumLobbyMembers(CSteamID steamId)
+        {
+            return 0;
+        }
+
+        public static int GetLobbyMemberLimit(CSteamID steamId)
+        {
+            return 0;
+        }
+
+        public static bool RequestLobbyData(CSteamID steamId)
+        {
+            return false;
+        }
+    }
+}
